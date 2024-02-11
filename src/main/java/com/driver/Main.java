cb82bef40b49957c4693a244da454af362d1ff53
Shrayansshjain <Shrayansshjain@gmail.com>
2024-02-11 14:58:32 +0530

    encapsulation done. getter and setter created

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        String val = "Shrayanssh";
+
+        // obj.name = val;            //name has only private access and can be accessed in RWOnly
+        //System.out.print(obj.name);
+
+        obj.setName(val);
+        obj.getName();
+    }
 }
